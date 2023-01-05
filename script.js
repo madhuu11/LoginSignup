9025e27440f0516b3e20bbc577235752ac62ad4f
Madhushree K <89026550+madhuu11@users.noreply.github.com>
2023-01-05 11:44:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
+
+function getData()
+{
+    //gettting the values
+    var username = document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+    //saving the values in local storage
+    localStorage.setItem("name", username);
+    localStorage.setItem("pass", password);
+}
